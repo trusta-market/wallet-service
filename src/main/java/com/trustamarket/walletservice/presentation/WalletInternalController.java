36a68ee0bc77949c77b98067337e5a1f7ce7a8d8
hil679 <hil679922@gmail.com>
2026-04-27 18:08:48 +0900

    fix: change controller response type

--- src/main/java/com/trustamarket/walletservice/presentation/WalletInternalController.java
+++ src/main/java/com/trustamarket/walletservice/presentation/WalletInternalController.java
@@ -2,11 +2,12 @@ package com.trustamarket.walletservice.presentation;
 
 import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trustamarket.common.response.CommonResponse;
 import com.trustamarket.walletservice.application.command.WalletCommandService;
 import com.trustamarket.walletservice.application.dto.result.CreateWalletResult;
 import com.trustamarket.walletservice.presentation.dto.response.CreateWalletResponse;
@@ -18,11 +19,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/internal/wallets")
 public class WalletInternalController {
 	private final WalletCommandService walletCommandService;
+
 	@PostMapping
-	public ResponseEntity<CreateWalletResponse> createWallet(UUID userId) { // return 타입과 파라미터 수정 필요
+	public CommonResponse<CreateWalletResponse> createWallet(UUID userId) { // return 타입과 파라미터 수정 필요
 
 		CreateWalletResult result = walletCommandService.createWallet(userId);
 
-		return new ResponseEntity<>(new CreateWalletResponse(result.walletId()), null); // common response로
+		return new CommonResponse(HttpStatus.CREATED.value(), new CreateWalletResponse(result.walletId()));
 	}
 }
