2111386e538e45ef9c8d41062e9ddbaeab59a223
hil679 <hil679922@gmail.com>
2026-04-27 18:21:36 +0900

    fix: WalletErrorCode로 정의한 것으로만 예외 관리하기 위해 생성자 삭제

--- src/main/java/com/trustamarket/walletservice/domain/exception/WalletException.java
+++ src/main/java/com/trustamarket/walletservice/domain/exception/WalletException.java
@@ -1,24 +1,10 @@
 package com.trustamarket.walletservice.domain.exception;
 
-import org.springframework.http.HttpStatus;
-
 import com.trustamarket.common.exception.CustomException;
 import com.trustamarket.common.exception.ErrorCodeSpec;
 
 public class WalletException extends CustomException {
 	public WalletException(ErrorCodeSpec errorCode) {
 		super(errorCode);
-	}
-
-	public WalletException(ErrorCodeSpec errorCode, HttpStatus expected) {
-		super(errorCode, expected);
-	}
-
-	public WalletException(HttpStatus status, String message) {
-		super(status, message);
-	}
-
-	public WalletException(HttpStatus status, String message, String field) {
-		super(status, message, field);
 	}
 }
