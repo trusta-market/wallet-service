5f41d2c2559b61549439dcd574933f31a3aa6a83
hil679 <hil679922@gmail.com>
2026-05-26 13:10:18 +0900

    feat: add hikari pool log

--- src/main/java/com/trustamarket/walletservice/WalletServiceApplication.java
+++ src/main/java/com/trustamarket/walletservice/WalletServiceApplication.java
@@ -2,12 +2,20 @@ package com.trustamarket.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import com.zaxxer.hikari.HikariConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class WalletServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WalletServiceApplication.class, args);
-	}
+		ConfigurableApplicationContext run = SpringApplication.run(WalletServiceApplication.class, args);
 
+		HikariConfig hikariConfig = run.getBean(HikariConfig.class);
+		log.info("[HIKARI Maximum PoolSize]" + hikariConfig.getMaximumPoolSize());
+	}
 }
