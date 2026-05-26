ef0ed6762a9aa9cc95937753c937f75e024acdaa
hil679 <hil679922@gmail.com>
2026-05-26 17:02:17 +0900

    fix: modify feignclient response

--- src/main/java/com/trustamarket/walletservice/wallet/infrastructure/payment/PaymentAdapter.java
+++ src/main/java/com/trustamarket/walletservice/wallet/infrastructure/payment/PaymentAdapter.java
@@ -2,13 +2,12 @@ package com.trustamarket.walletservice.wallet.infrastructure.payment;
 
 import java.util.UUID;
 
-import com.trustamarket.walletservice.wallet.application.dto.result.ChargePointResult;
-import com.trustamarket.walletservice.wallet.application.dto.result.CreateWalletResult;
-import com.trustamarket.walletservice.wallet.infrastructure.payment.dto.PaymentPointResponse;
 import org.springframework.stereotype.Component;
 
+import com.trustamarket.walletservice.wallet.application.dto.result.ChargePointResult;
 import com.trustamarket.walletservice.wallet.application.port.PaymentPort;
 import com.trustamarket.walletservice.wallet.infrastructure.payment.dto.PaymentPointRequest;
+import com.trustamarket.walletservice.wallet.infrastructure.payment.dto.PaymentPointResponse;
 import com.trustamarket.walletservice.wallet.infrastructure.payment.dto.WithdrawRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,7 @@ public class PaymentAdapter implements PaymentPort {
     @Override
     public ChargePointResult chargePoint(UUID userId, UUID pointTxRequestHistoryId, long chargeAmount) {
         PaymentPointRequest request = new PaymentPointRequest(userId, pointTxRequestHistoryId, chargeAmount);
-        PaymentPointResponse response = paymentFeignClient.paymentPoint(request).data();
+        PaymentPointResponse response = paymentFeignClient.paymentPoint(request).getBody().data(); //ResponseEntity -> CommonResponse의 body
         ChargePointResult result = new ChargePointResult(
                 response.paymentId(),
                 response.pointTxRequestHistoryId(),
