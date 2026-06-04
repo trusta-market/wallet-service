cefa624a5125154ea675128c7925ac2890717a28
hil679 <hil679922@gmail.com>
2026-06-05 03:40:30 +0900

    chore: add db migration file for shortage table

---
+++ src/main/resources/db/changelog/sql/V004__add_point_shortage.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+ALTER TABLE p_point_transaction_request_history
+    ADD COLUMN IF NOT EXISTS ref_id UUID;
+
+CREATE TABLE IF NOT EXISTS p_point_shortage (
+    id                                    UUID        PRIMARY KEY,
+    point_transaction_request_history_id  UUID        NOT NULL UNIQUE,
+    shortage                              BIGINT      NOT NULL,
+    CONSTRAINT fk_shortage_request_history
+        FOREIGN KEY (point_transaction_request_history_id)
+        REFERENCES p_point_transaction_request_history(point_transaction_request_history_id)
+);
