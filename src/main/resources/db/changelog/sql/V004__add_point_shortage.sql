f2037a23475f67002c65a83513266275b59332f3
hil679 <hil679922@gmail.com>
2026-06-05 13:12:44 +0900

    fix: add missing changeset header to V004 Liquibase migration

--- src/main/resources/db/changelog/sql/V004__add_point_shortage.sql
+++ src/main/resources/db/changelog/sql/V004__add_point_shortage.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 
+--changeset ihyein:1
 ALTER TABLE p_point_transaction_request_history
     ADD COLUMN IF NOT EXISTS ref_id UUID;
 
@@ -7,6 +8,7 @@ CREATE TABLE IF NOT EXISTS p_point_shortage (
     id                                    UUID        PRIMARY KEY,
     point_transaction_request_history_id  UUID        NOT NULL UNIQUE,
     shortage                              BIGINT      NOT NULL,
+    balance                               BIGINT      NOT NULL,
     CONSTRAINT fk_shortage_request_history
         FOREIGN KEY (point_transaction_request_history_id)
         REFERENCES p_point_transaction_request_history(point_transaction_request_history_id)
