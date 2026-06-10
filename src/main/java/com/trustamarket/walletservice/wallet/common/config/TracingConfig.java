b82861fc039e7d7fe9f71ebbc8cc015818f7b390
hil679 <hil679922@gmail.com>
2026-06-11 00:18:49 +0900

    fix: http2 problem because of zipkin

--- src/main/java/com/trustamarket/walletservice/wallet/common/config/TracingConfig.java
+++ src/main/java/com/trustamarket/walletservice/wallet/common/config/TracingConfig.java
@@ -1,9 +1,13 @@
 package com.trustamarket.walletservice.wallet.common.config;
 
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.aop.ObservedAspect;
+import java.net.http.HttpClient;
+
+import org.springframework.boot.actuate.autoconfigure.tracing.zipkin.ZipkinHttpClientBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 
 @Configuration
 public class TracingConfig {
@@ -11,5 +15,10 @@ public class TracingConfig {
     @Bean
     public ObservedAspect observedAspect(ObservationRegistry registry) {
         return new ObservedAspect(registry);
+    }
+
+    @Bean
+    public ZipkinHttpClientBuilderCustomizer zipkinHttp1Customizer() {
+        return builder -> builder.version(HttpClient.Version.HTTP_1_1);
     }
 }
